d53d32510055b204c43f1615197d31678cf307be
Calvin Wu <calvinwu0716@gmail.com>
2021-09-04 15:12:58 -0700

    First Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import AppBar from './@matrial-ui/core/AppBar'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <AppBar></AppBar>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
