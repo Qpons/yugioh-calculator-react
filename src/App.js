63103f1b4a49456851360e844cf2b4a24949772a
Calvin Wu <calvinwu0716@gmail.com>
2021-09-05 14:37:16 -0700

    Add game end logic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { DamageContext } from './DamageContext';
 import { GameOverContext } from './DamageContext';
@@ -13,6 +13,19 @@ function App() {
   const [damage, setDamage] = useState('');
   const [gameOver, setGameOver] = useState(0);
   const [matchProgress, setMatchProgress] = useState([]);
+
+  useEffect(() => {
+    console.log(matchProgress.length);
+    if (matchProgress.length === 3) {
+      alert('Winner is ' + matchProgress[2]);
+      setMatchProgress([]);
+    } else if (matchProgress.length === 2) {
+      if (matchProgress[0] === matchProgress[1]) {
+        alert('Winner is ' + matchProgress[0]);
+        setMatchProgress([]);
+      }
+    }
+  }, [matchProgress.length]);
 
   return (
     <div>
